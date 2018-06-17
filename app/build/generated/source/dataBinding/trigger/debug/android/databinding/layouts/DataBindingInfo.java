2e78cdf717beb1c567110fbae43fdb4a2aefd1f3
sjugurdzija <jugurdzija.sasa@gmail.com>
2018-06-18 00:11:11 +0200

    Initial

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="fbf9ed0d-526e-4672-9a91-4add59522b76")
+public class DataBindingInfo {}
